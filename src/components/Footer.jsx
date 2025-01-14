e7e6cceb14ed046cb5a5f9fa3b2bb002e47ce9b7
Ali Mahdi <alimahdi@Alis-MacBook-Pro.local>
2025-01-14 15:59:05 -0600

    Fix GitHub Pages routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,6 @@
  */
 import { ButtonPrimary } from "../components/Button"
 import { Link } from 'react-router-dom';
-import Skill from "../pages/Skills";
 
 const sitemap = [
     {
@@ -82,7 +81,7 @@ const Footer = () => {
                                             to={href}
                                             className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                                         >
-                                            <Skills />
+                                            {label}
                                         </Link>
                                         
                                     </li>
