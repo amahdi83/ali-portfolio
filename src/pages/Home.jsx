14c7c5e9f705abaa460c5f49728c67580d566e7c
Ali Mahdi <alimahdi@Alis-MacBook-Pro.local>
2025-01-14 02:52:23 -0600

    Fix GitHub Pages routing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ const aboutItems = [
           <div>
             <div className="flex items-center gap-3">
               <figure className="img-box w-9 h-9 rounded-xl">
-                <img src="./ali-portfolio/images/avatar-1.jpg" width={40} height={40} alt="Ali Mahdi portrait" className="img-cover" />
+                <img src="images/avatar-1.jpg" width={40} height={40} alt="Ali Mahdi portrait" className="img-cover" />
               </figure>
 
               <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
